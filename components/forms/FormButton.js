e17cc3b6edc7c7589dcf75973ea8a78597adb399
Cristiano Amici <amicicristiano@gmail.com>
2021-05-16 17:05:50 -0500

    Style up FormButton component

--- components/forms/FormButton.js
+++ components/forms/FormButton.js
@@ -1,10 +1,13 @@
 import PropTypes from 'prop-types';
+import classNames from 'classnames';
 
 function FormButton({
-  onClick, text, primary, ...otherProps
+  onClick, className, text, primary, ...otherProps
 }) {
+  const classes = classNames('form-button', { primary }, className);
+
   return (
-    <button {...otherProps} type="button">
+    <button {...otherProps} className={classes} type="button" onClick={onClick}>
       {text}
     </button>
   );
@@ -12,11 +15,13 @@ function FormButton({
 
 FormButton.propTypes = {
   onClick: PropTypes.func.isRequired,
+  className: PropTypes.string,
   text: PropTypes.string.isRequired,
   primary: PropTypes.bool,
 };
 
 FormButton.defaultProps = {
+  className: '',
   primary: false,
 };
 
