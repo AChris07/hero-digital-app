64d1c9755ae858d16ccbdbbc5dd82c1e10766456
Cristiano Amici <amicicristiano@gmail.com>
2021-05-18 23:27:59 -0500

    Refactor and style up FormSelect
    To be able to include a custom select with stylable menu
    in a quick manner, we implemented React Select.
    
    As a compromise between using React Select and A11Y, we styled
    FormSelect as a hybrid component, with a <select> for non-visual access
    and keyboard handling, and React Select for mouse handling.

--- components/forms/FormSelect.js
+++ components/forms/FormSelect.js
@@ -1,30 +1,75 @@
+import Select, { components } from 'react-select';
 import { useUIDSeed } from 'react-uid';
 import PropTypes from 'prop-types';
+import classNames from 'classnames';
+
+import ExpandArrow from '../../public/text-expand-arrow.svg';
 
 function FormSelect({
-  onChange, label, options, value, ...otherProps
+  onChange,
+  className,
+  label,
+  options,
+  placeholder,
+  value,
+  keepOpen,
+  ...otherProps
 }) {
   const seed = useUIDSeed();
   const labelComponent = label && (
-    <label htmlFor={seed('input')}>{label}</label>
+    <label htmlFor={seed('input')} className="form-select__label">
+      {label}
+    </label>
+  );
+  const classes = classNames('form-select__container', className);
+  const DropdownIndicator = (props) => (
+    <components.DropdownIndicator {...props}>
+      <ExpandArrow />
+    </components.DropdownIndicator>
   );
 
   return (
-    <div {...otherProps}>
+    <div {...otherProps} className={classes}>
       {labelComponent}
-      <select id={seed('input')} onChange={onChange} value={value}>
-        {options.map((opt) => (
-          <option key={seed(opt)} value={opt.value}>
-            {opt.label}
+      <div className="form-select__wrapper">
+        <select
+          id={seed('input')}
+          className="form-select__native"
+          onChange={onChange}
+          value={value}
+        >
+          <option key={seed('empty-option')} disabled selected>
+            {placeholder}
           </option>
-        ))}
-      </select>
+          {options.map((opt) => (
+            <option key={seed(opt)} value={opt.value}>
+              {opt.label}
+            </option>
+          ))}
+        </select>
+        <ExpandArrow className="form-select__native-arrow" />
+        <div className="form-select__custom__container">
+          <Select
+            id={seed('input')}
+            className="form-select__custom"
+            classNamePrefix="form-select__custom"
+            options={options}
+            onChange={onChange}
+            value={value}
+            components={{ DropdownIndicator }}
+            isSearchable={false}
+            placeholder={placeholder}
+            menuIsOpen={keepOpen || undefined}
+          />
+        </div>
+      </div>
     </div>
   );
 }
 
 FormSelect.propTypes = {
   onChange: PropTypes.func.isRequired,
+  className: PropTypes.string,
   label: PropTypes.string,
   options: PropTypes.arrayOf(
     PropTypes.shape({
@@ -32,12 +77,17 @@ FormSelect.propTypes = {
       value: PropTypes.string,
     }),
   ).isRequired,
+  placeholder: PropTypes.string,
   value: PropTypes.string,
+  keepOpen: PropTypes.bool,
 };
 
 FormSelect.defaultProps = {
+  className: '',
   label: undefined,
+  placeholder: '- Select One -',
   value: undefined,
+  keepOpen: false,
 };
 
 export default FormSelect;
--- stories/FormSelect.stories.js
+++ stories/FormSelect.stories.js
@@ -21,6 +21,45 @@ WithLabel.args = {
   ],
 };
 
+export const WithPlaceholder = Template.bind({});
+WithPlaceholder.args = {
+  label: 'Are you a US Resident?',
+  placeholder: 'Are you?',
+  options: [
+    { label: 'Yes', value: 'yes' },
+    { label: 'No', value: 'no' },
+  ],
+};
+
+export const Expanded = Template.bind({});
+Expanded.args = {
+  label: 'Are you a US Resident?',
+  options: [
+    { label: 'Yes', value: 'yes' },
+    { label: 'No', value: 'no' },
+  ],
+  keepOpen: true,
+};
+
+const generateOptions = (max) => {
+  const options = [];
+  for (let i = 1; i <= max; i += 1) {
+    options.push({
+      label: i.toString(),
+      value: i.toString(),
+    });
+  }
+
+  return options;
+};
+
+export const ScrollableOptions = Template.bind({});
+ScrollableOptions.args = {
+  label: 'Pick a number',
+  options: generateOptions(100),
+  keepOpen: true,
+};
+
 export const WithoutLabel = Template.bind({});
 WithoutLabel.args = {
   options: [
