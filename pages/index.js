e598a6ce7d65d109dd803b1133308cd9bf15b545
Cristiano Amici <amicicristiano@gmail.com>
2021-05-19 00:19:25 -0500

    Move background texture to <main>
    Prevent background from affecting Storybook

--- pages/index.js
+++ pages/index.js
@@ -16,46 +16,49 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <header>
-        <h1>Sign up for email updates</h1>
-        <p>*Indicates a required field</p>
-      </header>
+      <main>
+        <header>
+          <h1>Sign up for email updates</h1>
+          <p>*Indicates a required field</p>
+        </header>
 
-      <form type="submit">
-        <div className="columns is-multiline">
-          <FormInput className="column is-half" label="First Name" />
-          <FormInput className="column is-half" label="Last Name" />
-          <FormInput className="column is-half" label="Email Address" />
-          <FormInput className="column is-half" label="Organization" />
-          <FormSelect
-            className="column is-half"
-            label="EU Resident"
-            options={[
-              { label: 'Yes', value: 'yes' },
-              { label: 'No', value: 'no' },
-            ]}
-          />
+        <form type="submit">
+          <div className="columns is-multiline">
+            <FormInput className="column is-half" label="First Name" />
+            <FormInput className="column is-half" label="Last Name" />
+            <FormInput className="column is-half" label="Email Address" />
+            <FormInput className="column is-half" label="Organization" />
+            <FormSelect
+              className="column is-half"
+              label="EU Resident"
+              options={[
+                { label: 'Yes', value: 'yes' },
+                { label: 'No', value: 'no' },
+              ]}
+            />
 
-          <FormCheck className="column is-half" label="Advances" />
-          <FormCheck className="column is-half" label="Alerts" />
-          <FormCheck className="column is-half" label="Other Communications" />
-        </div>
+            <FormCheck className="column is-half" label="Advances" />
+            <FormCheck className="column is-half" label="Alerts" />
+            <FormCheck
+              className="column is-half"
+              label="Other Communications"
+            />
+          </div>
 
-        <div className="columns">
-          <FormButton
-            className="column is-half"
-            onClick={() => {}}
-            text="Submit"
-          />
-          <FormButton
-            className="column is-half"
-            onClick={() => {}}
-            text="Reset"
-          />
-        </div>
-      </form>
-
-      <footer />
+          <div className="columns">
+            <FormButton
+              className="column is-half"
+              onClick={() => {}}
+              text="Submit"
+            />
+            <FormButton
+              className="column is-half"
+              onClick={() => {}}
+              text="Reset"
+            />
+          </div>
+        </form>
+      </main>
     </>
   );
 }
