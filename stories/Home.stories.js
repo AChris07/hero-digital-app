05a2e35052b592f86d1817f812f8d8bed5e58dfa
Cristiano Amici <amicicristiano@gmail.com>
2021-05-19 22:28:44 -0500

    Fix Home story
    Wrap the component with Store Provider

--- stories/Home.stories.js
+++ stories/Home.stories.js
@@ -1,13 +1,19 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 
 import Home from '../pages/index';
+import store from '../store';
 
 export default {
   title: 'Pages/Home',
   component: Home,
 };
 
-const Template = (args) => <Home {...args} />;
+const Template = (args) => (
+  <Provider store={store}>
+    <Home {...args} />
+  </Provider>
+);
 
 export const Default = Template.bind({});
 Default.args = {};
